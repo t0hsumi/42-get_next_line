066d6e2b4d7dee40bdb23eea7d520b4811ffe466
taito ohsumi <dajiaotaito18@gmail.com>
2021-06-23 11:34:08 +0900

    add simple test & test cases

---
+++ simplemain.c
@@ -0,0 +1,24 @@
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(void){
+	int fd;
+	char *line;
+	int ret;
+
+	fd = open("sample7.txt", O_RDONLY);
+	do
+	{
+		ret = get_next_line(fd, &line);
+		if (ret)
+			printf("%s\n", line);
+		else if (!ret)
+			printf("%s", line);
+		free(line);
+	} while (ret);
+	close(fd);
+	return(0);
+}
