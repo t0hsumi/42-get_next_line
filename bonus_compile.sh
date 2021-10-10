c80dcf7b11d63965e1e58dc52909c112690df056
taito ohsumi <dajiaotaito18@gmail.com>
2021-10-10 11:58:22 +0900

    delete test file

--- bonus_compile.sh
+++
@@ -1,1 +0,0 @@
-gcc -Wall -Wextra -Werror -D BUFFER_SIZE="$1" main.c get_next_line_bonus.c get_next_line_utils_bonus.c
--- mandatory_compile.sh
+++
@@ -1,1 +0,0 @@
-gcc -Wall -Wextra -Werror -D BUFFER_SIZE="$1" main.c get_next_line.c get_next_line_utils.c
